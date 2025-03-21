0e3fc556b93db509331b3c75e405e62f9e40ab23
Dilip Chauhan <chauhandilip305@gmail.com>
2025-03-22 00:09:14 +0530

    added few highlighting groups

--- lua/gruvbox-material/groups.lua
+++ lua/gruvbox-material/groups.lua
@@ -145,6 +145,8 @@ function groups.get(contrast)
     Whitespace = { fg = colors.bg5 }, -- '~' and '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line).
     SpecialKey = { fg = colors.bg5 }, -- Meta and special keys listed with ":map", also for text used to show unprintable characters in the text, 'listchars'. Generally: text that is displayed differently from what it really is.
     Title = { fg = colors.orange, bold = true }, -- titles for output from ":set all", ":autocmd" etc.
+    Border = { fg = colors.cyan },
+    WinSeparator = { fg = colors.bg5 },
 
     -- Popup menu
     Pmenu = { fg = colors.fg1, bg = colors.bg3 }, -- normal item
@@ -155,7 +157,8 @@ function groups.get(contrast)
     Normal = { fg = colors.fg0, bg = colors.bg0 }, -- normal text
     NormalNC = { link = "Normal" },
     NormalFloat = { fg = colors.fg1, bg = colors.bg3 },
-    FloatTitle = { fg = colors.red },
+    FloatTitle = { link = "Title" },
+    FloatBorder = { link = "Border" },
 
     Question = { fg = colors.yellow }, -- hit-enter prompt and yes/no questions
 
