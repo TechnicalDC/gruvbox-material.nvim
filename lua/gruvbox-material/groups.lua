ba04890c737a46565696e979ad02daa6af38b90d
Dilip Chauhan <chauhandilip305@gmail.com>
2025-04-14 00:03:34 +0530

    updated

--- lua/gruvbox-material/groups.lua
+++ lua/gruvbox-material/groups.lua
@@ -691,6 +691,7 @@ function groups.get(contrast)
       -- folke/snacks.nvim
       SnacksPickerFile               = { fg = colors.fg0 },
       SnacksBackdrop                 = { link = "Normal" },
+      SnacksBackdrop_000000          = { link = "Normal" },
       SnacksPickerBold               = { link = "Bold"                        },
       SnacksPickerItalic             = { link = "Italic"                      },
       SnacksPickerGitScope           = { link = "Italic"                      },
